ca95bf204dda6379a9a61b819982e97089027ab7
Lilian <dev@lil.re>
2024-11-12 23:12:47 +0100

    Add local SQLite DB, encryption, and external DB connections

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,116 @@
+#[macro_use] extern crate magic_crypt;
+
+use magic_crypt::MagicCryptTrait;
+use tauri_plugin_sql::{Migration, MigrationKind};
+use mysql::*;
+use mysql::prelude::*;
+use serde_json::json;
+use serde_json::Value;
+
+#[tauri::command]
+fn encrypt_string(value: &str) -> String {
+    let mcrypt = new_magic_crypt!("magickey", 256);
+    mcrypt.encrypt_str_to_base64(value)
+}
+
+#[tauri::command]
+fn decrypt_string(value: &str) -> String {
+    let mcrypt = new_magic_crypt!("magickey", 256);
+    mcrypt.decrypt_base64_to_string(&value).unwrap()
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn read_mysql_database() -> Result<Value, String> {
+    let database_url = "mysql://test:test@localhost:3306/coin_wombat_db";
+
+    // Create a connection to the database
+    let pool = match Pool::new(database_url) {
+        Ok(p) => p,
+        Err(e) => return Err(format!("Database connection error: {}", e)),
+    };
+
+    let mut conn = match pool.get_conn() {
+        Ok(c) => c,
+        Err(e) => return Err(format!("Failed to get connection: {}", e)),
+    };
+
+    // Execute the query and fetch the results
+    let result = conn.query_iter("SELECT id, symbol, label from coin");
+
+    match result {
+        Ok(mut result_set) => {
+            let mut rows = Vec::new();
+
+            // Iterate through each row in the result set
+            while let Some(result_row) = result_set.next() {
+                match result_row {
+                    Ok(row) => {
+                        let mut json_row = serde_json::Map::new();
+
+                        // Populate json_row with each column value in the row
+                        for (index, column) in row.columns_ref().iter().enumerate() {
+                            let column_name = column.name_str().to_string();
+
+                            // Use get_opt to handle different types
+                            let column_value: Value = match row.get_opt(index) {
+                                Some(Ok(Some(val))) => {
+                                    // Convert to serde_json::Value based on type
+                                    match val {
+                                        mysql::Value::Int(i) => serde_json::json!(i),
+                                        mysql::Value::UInt(u) => serde_json::json!(u),
+                                        mysql::Value::Float(f) => serde_json::json!(f),
+                                        mysql::Value::Double(d) => serde_json::json!(d),
+                                        mysql::Value::Bytes(b) => serde_json::json!(String::from_utf8_lossy(b.as_slice())),
+                                        mysql::Value::NULL => Value::Null,
+                                        _ => Value::Null,
+                                    }
+                                }
+                                _ => Value::Null,
+                            };
+
+                            json_row.insert(column_name, column_value);
+                        }
+
+                        rows.push(Value::Object(json_row));
+                    }
+                    Err(e) => return Err(format!("Error fetching row: {}", e)),
+                }
+            }
+
+            Ok(Value::Array(rows))
+        },
+        Err(e) => Err(format!("Failed to execute query: {}", e)),
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create users table",
+            sql: "CREATE TABLE IF NOT EXISTS users (  
+                id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                name TEXT NOT NULL,  
+                email TEXT  
+            )",
+            kind: MigrationKind::Up,
+        }
+    ];
+
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:test.db", migrations)
+                .build()
+        )
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet, encrypt_string, decrypt_string, read_mysql_database])
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,153 @@
+import { useEffect, useState } from "react";
+import {invoke} from "@tauri-apps/api/core";
+import Database from "@tauri-apps/plugin-sql";
+import "./App.css";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+function App() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  async function getUsers() {
+    try {
+      const db = await Database.load("sqlite:test.db");
+      const dbUsers = await db.select<User[]>("SELECT * FROM users");
+      const decryptedUsers = dbUsers.map(async (user) => {
+        const email = await invoke('decrypt_string', { value: user.email })
+        return {
+          ...user,
+          email: `${email}`
+        }
+      })
+      const users = await Promise.all(decryptedUsers)
+      console.log(users)
+
+      setError("");
+      setUsers(users);
+      setIsLoadingUsers(false);
+    } catch (error) {
+      console.log(error);
+      setError("Failed to get users - check console");
+    }
+  }
+
+  async function setUser(user: Omit<User, "id">) {
+    try {
+      setIsLoadingUsers(true);
+      const db = await Database.load("sqlite:test.db");
+      const email = await invoke("encrypt_string", { value: user.email })
+
+      await db.execute("INSERT INTO users (name, email) VALUES ($1, $2)", [
+        user.name,
+        email,
+      ]);
+
+      getUsers().then(() => setIsLoadingUsers(false));
+    } catch (error) {
+      console.log(error);
+      setError("Failed to insert user - check console");
+    }
+  }
+
+  async function removeUser(id: number) {
+    try {
+      setIsLoadingUsers(true);
+      const db = await Database.load("sqlite:test.db");
+
+      await db.execute("DELETE FROM users WHERE id = $1", [id]);
+
+      getUsers().then(() => setIsLoadingUsers(false));
+    } catch (error) {
+      console.log(error);
+      setError("Failed to remove user - check console");
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function testMySqlDb() {
+    const result = await invoke("read_mysql_database")
+    console.log(result)
+  }
+
+  return (
+      <main className="container">
+        <h1>Welcome to Tauri + SQLite</h1>
+
+        {isLoadingUsers ? (
+            <div>Loading users...</div>
+        ) : (
+          <div style={{display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+              <form
+                  className="row"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    setUser({ name, email });
+                  }}
+              >
+                <input
+                    id="name-input"
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    placeholder="Enter a name..."
+                />
+                <input
+                    type="email"
+                    id="email-input"
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                    placeholder="Enter an email..."
+                />
+                <button type="submit">Add User</button>
+              </form>
+
+            <div style={{display: "flex", flexDirection: "column", gap: "2rem"}}>
+              <button onClick={testMySqlDb}>Test MySQL DB</button>
+            </div>
+            <div style={{display: "flex", flexDirection: "column", gap: "2rem" }}>
+                <h1>Users</h1>
+                <table>
+                  <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Remove</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {users.map((user) => (
+                      <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>
+                          <button onClick={(e) => {
+                            e.preventDefault();
+                            removeUser(user.id);
+                          }}>
+                            Remove User
+                          </button>
+                        </td>
+                      </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+        )}
+
+        {error && <p>{error}</p>}
+      </main>
+  );
+}
+
+export default App;
